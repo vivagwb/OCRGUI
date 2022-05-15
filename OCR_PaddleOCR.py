e76523518393bb81812c9bf5223c74de95a29a8a
vivagwb <gwb1990@126.com>
2022-05-15 23:26:44 +0800

    paddle ocr v0.2

--- OCR_PaddleOCR.py
+++ OCR_PaddleOCR.py
@@ -1,25 +1,28 @@
 from paddleocr import  PaddleOCR,draw_ocr
 from tkinter import  *
 from tkinter.filedialog import askopenfilename
-from threading import Thread
+#from threading import Thread
+
+
+
 
 
 class GUI(Frame):
     def __init__(self,parent=None, **kwargs):
         Frame.__init__(self,parent,**kwargs)
-        self.config(height=500,width=300,bd="whitesmoke")
+        self.config(height=500,width=300,bg="whitesmoke")
         self.pack()
         self.var=StringVar()
-        openfile=lambda buttonReturn=askopenfilename:self.fileOCR(buttonReturn)
-        FileAcquireButton=Button(self,text="本地图片识别",command=openfile)
-        FileAcquireButton.pack(side=LEFT,expand=YES)
+        #openfile=lambda buttonReturn=askopenfilename:self.fileOCR(buttonReturn)
+        FileAcquireButton=Button(self,text="本地图片识别",command=self.fileOCR)
+        FileAcquireButton.pack(side=TOP,expand=YES)
         contentMessage=Message(self,textvariable=self.var,font="times")
-        contentMessage.pack(side=BOTTOM,expand=YES)
+        contentMessage.pack(side=BOTTOM,expand=YES,fill=X)
 
 
-    def fileOCR(self,filename):
+    def fileOCR(self):
         contentR=[]
-        filepath=filename()
+        filepath=askopenfilename()
         print(filepath)
         ocr=PaddleOCR(use_angle_cls=True)
         result=ocr.ocr(filepath,cls=True)
