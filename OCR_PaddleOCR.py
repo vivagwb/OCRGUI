bc71fe4357116520bcc12225ff5ebe0fcfcb8063
vivagwb <gwb1990@126.com>
2022-05-17 23:21:40 +0800

    paddle ocr 实现了通过剪贴板读取图片并识别

--- OCR_PaddleOCR.py
+++ OCR_PaddleOCR.py
@@ -1,4 +1,4 @@
-from paddleocr import  PaddleOCR,draw_ocr
+from paddleocr import  PaddleOCR
 from tkinter import  *
 from tkinter.filedialog import askopenfilename
 from tkinter.scrolledtext import ScrolledText
@@ -7,8 +7,10 @@ import queue
 import time
 import filetype
 import pyperclip
-
-
+from PIL import ImageGrab
+from PIL import Image
+import numpy as np
+import os
 
 
 
@@ -20,23 +22,26 @@ class GUI(Frame):
         self.dataqueue=queue.Queue()
         #self.var=StringVar()
         #openfile=lambda buttonReturn=askopenfilename:self.fileOCR(buttonReturn)
-        self.FileOCRButton=Button(self,text="本地图片识别",command=self.makethread)
-        self.FileOCRButton.pack(side=TOP,expand=YES,fill=X)
+        self.FileOCRButton=Button(self,text="本地图片识别",command=self.filemakethread)
+        self.FileOCRButton.grid(row=0,sticky=E,padx=15,pady=3)
+        self.ClipOCRButton=Button(self,text="剪贴板图片识别",command=self.clipmakethread)
+        self.ClipOCRButton.grid(row=0,sticky=W,padx=15,pady=3)
         self.content=ScrolledText(self)
-        self.content.pack(side=BOTTOM,expand=YES,fill=BOTH)
+        self.content.grid(row=1,sticky=W+E+N+S)
+        #self.content.pack(side=BOTTOM,expand=YES,fill=BOTH)
         self.contentDisplay()
 
 
 
     def contentDisplay(self):
         try:
-            data=self.dataqueue.get(block=False)
+            data=self.dataqueue.get(block=True)
         except queue.Empty:
             pass
         else:
             self.content.insert('end',"%s\n" % str(data))
-        self.content.see('end')
-        self.content.after(100,self.contentDisplay)
+            self.content.see('end')
+        self.content.after(50,self.contentDisplay)
 
 
     def fileOCR(self):
@@ -44,7 +49,8 @@ class GUI(Frame):
         filepath = askopenfilename()
         filekind=filetype.guess_extension(filepath)
         if filekind in ['jpg','png','bmp']:
-            self.Avaialfilepath=filepath
+            filename=os.path.split(filepath)[1]
+            self.dataqueue.put("已获取本地图片文件:%s \n" % filename )
             contentR = []
             ocr = PaddleOCR(use_angle_cls=True)
             result = ocr.ocr(filepath, cls=True)
@@ -63,9 +69,37 @@ class GUI(Frame):
             self.dataqueue.put("文件不是图片，无法识别!")
 
 
+    def clipOCR(self):
+        self.content.delete(1.0, "end")
+        im=ImageGrab.grabclipboard()
+        if isinstance(im,Image.Image):
+            imdata=np.array(im)
+            print(im.format, im.size, im.mode)
+            self.dataqueue.put("已读取剪贴板图片,图片格式%s,图片大小%s，图片模式%s \n" % (im.format,im.size,im.mode))
+            contentR = []
+            ocr = PaddleOCR(use_angle_cls=True)
+            result = ocr.ocr(imdata, cls=True)
+            for line in result:
+                contentR.append(line[1][0])
+            self.ocrcontent = '\n'.join(contentR)
+            print(contentR)
+            self.dataqueue.put("%s" % self.ocrcontent)
+            # 把内容写入剪贴板
+            try:
+                pyperclip.copy(self.ocrcontent)
+                self.dataqueue.put("\n识别内容写入剪贴板")
+            except:
+                self.dataqueue.put("\n 识别内容写入剪贴板失败")
+        else:
+            self.dataqueue.put("剪贴板无图片！")
 
-    def makethread(self):
+    def filemakethread(self):
         Thread(target=self.fileOCR,args=()).start()
+
+    def clipmakethread(self):
+        Thread(target=self.clipOCR,args=()).start()
+
+
 
 
 
